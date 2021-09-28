8dfa89380de5d2fc1d4d1a13f2cf820078ff64cd
Chauhan-Anshu <91522014+Chauhan-Anshu@users.noreply.github.com>
2021-09-28 14:30:34 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,30 +1,30 @@
-var c=180;
+var c=30;
         var t;
         timedCount();
   function timedCount()
-	{
-		var hours = parseInt( c / 3600 ) % 24;
-        	var minutes = parseInt( c / 60 ) % 60;
-        	var seconds = c % 60;
-        	var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
-        	$('#timer').html(result);
-            	if(c == 0 )
-		          {
-                	$("#userForm").submit();
-			            window.location="submit.php";
-		          }
-            	c = c - 1;
-            	t = setTimeout(function()
-		          {
-			 timedCount()
-		},1000);
+  {
+    var hours = parseInt( c / 3600 ) % 24;
+          var minutes = parseInt( c / 60 ) % 60;
+          var seconds = c % 60;
+          var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+          $('#timer').html(result);
+              if(c == 0 )
+              {
+                  $("#userForm").submit();
+                  window.location="submit.php";
+              }
+              c = c - 1;
+              t = setTimeout(function()
+              {
+       timedCount()
+    },1000);
         }
 
   $(function() {
     $('#userForm').captcha();
   })
   $(document).ready(function(){
-    $("#captchaInput").prop('placeholder','Put Captcha solution');
+    $("#captchaInput").prop('placeholder','Put captcha solution');
     $("#captchaInput").keyup(function(){
       var cap=$(this).val();
       var ccap=$('#captchaText').text();
@@ -45,7 +45,6 @@ var c=180;
         }
     });
     $("#userForm").submit(function(){
-      alert("submit");
       window.location="submit.php";
     });
   });
